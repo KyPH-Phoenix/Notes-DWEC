8d95acf7c7d9f20bbcb011a72999f4bc63989ad1
hugon <asdf@>
2023-03-12 19:16:25 +0100

    register done

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -2,37 +2,53 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-const doRegister = () => {
-    DoRegister();
-}
-
-const DoRegister = () => {
-    let token = localStorage.getItem("tokemn")
+export default function Register() {
+    let token = localStorage.getItem("toke  n")
     const navigate = useNavigate();
 
-    useEffect(() => {
-        if (token) navigate("/notes")    
-    }, navigate);
-}
+    const expDate = new Date(localStorage.getItem("expiration"));
 
-export default function Register() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    if (token && expDate.getTime() > new Date().getTime()) {
+        navigate("/notes");
+    }
+    
+    const fetchRegister = async (event) => {
+        event.preventDefault();
+        console.log(password, username);
+
+        const response = await fetch("http://localhost:8081/signup", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            }),
+        });
+    }
+
+
     return (
         <main>
             <h1>Register</h1>
-            <form>
+            <form onSubmit={fetchRegister}>
                 <section>
                     <label htmlFor="username">Username: </label>
-                    <input type={"Text"} name={"username"}></input>
+                    <input name="username" type="text" id="username" placeholder="Introduce un nombre de usuario" onChange={(input) => { setUsername(input.target.value.toLowerCase().trim()); }} />
                 </section>
 
                 <section>
                     <label htmlFor="password">Password: </label>
-                    <input type={"Password"} name={"password"}></input>
+                    <input name="password" type="password" id="password" placeholder="Introduce una contraseña" onChange={(input) => { setPassword(input.target.value.trim()); }}></input>
                 </section>
 
-                <button onClick={doRegister()}>Register</button>
+                <input type="submit" value="Register"></input>
             </form>
-            
+
             <p>Already have an account? Login <Link to={"/login"}>here</Link>.</p>
         </main>
     );
